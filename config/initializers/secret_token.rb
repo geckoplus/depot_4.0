254c1096c7dcbc581ed416f5e91528a6d4882920
geckoplus <pavlifestyle@gmail.com>
2014-05-12 11:56:14 +0200

    upload all files to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '554a554f63a74f530e5dd73c7888127d8332b45dafb7770600e7dc8edb30da41b0ef1b0810eb8a0967df88e7cc874ae34c8fd4fe53ecec2a024bff514a72fa72'
